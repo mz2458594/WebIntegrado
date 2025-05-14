a6d21736da4b5e52e7df0872cceae525268d8761
mz2458594 <mz2458594@gmail.com>
2025-05-13 20:45:39 -0500

    Eliminando import innecesarios

--- src/main/java/com/example/domain/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/example/domain/ecommerce/config/SecurityConfig.java
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import com.example.domain.ecommerce.security.CustomSuccessHandler;
-import com.example.domain.ecommerce.services.CustomUserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -46,7 +44,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
