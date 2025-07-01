e17e4a93cafef861abdf93123ffe06ce1ac3d13b
mz2458594 <mz2458594@gmail.com>
2025-07-01 17:36:02 -0500

    Corrigiendo carrito de compra

--- src/main/java/com/example/domain/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/domain/ecommerce/EcommerceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceApplication {
-
+	
 	public static void main(String[] args) {
 		//EJECUCION DEL PROYECTO
 		SpringApplication.run(EcommerceApplication.class, args); 
