add3612cdb6ad552b6f715865e520a8ae180a581
mz2458594 <mz2458594@gmail.com>
2025-07-12 15:58:54 -0500

    Notifiacion de pedidos

--- src/main/java/com/example/domain/ecommerce/controllers/web/GlobalModelAtributte.java
+++ src/main/java/com/example/domain/ecommerce/controllers/web/GlobalModelAtributte.java
@@ -1,6 +1,9 @@
 package com.example.domain.ecommerce.controllers.web;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,8 +15,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import com.example.domain.ecommerce.models.entities.Cliente;
 import com.example.domain.ecommerce.models.entities.Empleado;
 import com.example.domain.ecommerce.models.entities.Notificacion;
+import com.example.domain.ecommerce.models.entities.Pedido;
 import com.example.domain.ecommerce.models.entities.Producto;
+import com.example.domain.ecommerce.models.enums.EstadoPedido;
 import com.example.domain.ecommerce.services.ProductoService;
+import com.example.domain.ecommerce.services.PedidoService;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -22,6 +28,9 @@ public class GlobalModelAtributte {
 
     @Autowired
     private ProductoService productoService;
+
+    @Autowired
+    private PedidoService pedidoService;
 
     @ModelAttribute
     public void addUserModel(HttpSession session, Model model) {
@@ -35,12 +44,28 @@ public class GlobalModelAtributte {
             model.addAttribute("empleado", empleado);
             List<Producto> productos = productoService.listarProducto();
 
-            List<Notificacion> notificaciones = productos.stream()
-                    .filter(p -> Integer.parseInt(p.getStock()) < 5)
-                    .map(p -> new Notificacion("El producto " + p.getNombre() + "tiene stock bajo (" + p.getStock() + " unidades)", LocalDateTime.now()))
-                    .collect(Collectors.toList());
+            List<Pedido> pedidos = pedidoService.getPedidos();
 
-            model.addAttribute("notificaciones", notificaciones);
+            List<Notificacion> notificacions = new ArrayList<>();
+
+            notificacions.addAll(pedidos.stream()
+                    .filter(p -> p.getEstado() == EstadoPedido.PENDIENTE)
+                    .filter(p -> {
+                        LocalDate fechaPedido = p.getFechaPedido().toLocalDateTime().toLocalDate();
+                        long dias = ChronoUnit.DAYS.between(fechaPedido, LocalDate.now());
+                        return dias == 1 || dias == 2;
+                    })
+                    .map(p -> new Notificacion("El pedido con ID " + p.getIdPedido() + " con estado PENDIENTE va a cumplir el límite de confirmación", LocalDateTime.now()))
+                    .collect(Collectors.toList()));
+
+            notificacions.addAll(productos.stream()
+                    .filter(p -> Integer.parseInt(p.getStock()) < 5)
+                    .map(p -> new Notificacion(
+                            "El producto " + p.getNombre() + "tiene stock bajo (" + p.getStock() + " unidades)",
+                            LocalDateTime.now()))
+                    .collect(Collectors.toList()));
+
+            model.addAttribute("notificaciones", notificacions);
         }
     }
 
